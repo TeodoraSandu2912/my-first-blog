74c5f9ddde3c5fd4b140678eaefac7c8732afad9
Sandu Teodora <sandu.teodora2912@gmail.com>
2020-06-30 01:15:31 +0300

    Changed settings.py

--- bridgingcoursework/settings.py
+++ bridgingcoursework/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
