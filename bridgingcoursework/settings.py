646fe9b7047b55b6528ee6666c339902a6be6367
Sandu Teodora <sandu.teodora2912@gmail.com>
2020-07-02 02:11:23 +0300

    Solved static imports

--- bridgingcoursework/settings.py
+++ bridgingcoursework/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
